d470630b7459d16b339f7a39472c73bc85465df4
ernestoacosta75 <ernestoacostacuba@yahoo.com.mx>
2025-04-05 12:15:23 +0200

    Adding explanation for the use of Hibernate5Module

--- src/main/java/microservices/book/multiplication/configuration/AppConfig.java
+++ src/main/java/microservices/book/multiplication/configuration/AppConfig.java
@@ -13,6 +13,17 @@ public class AppConfig {
         return new Random();
     }
 
+    /**
+     * Since entities like User will be fetched in LAZY mode,
+     * and because Hibernate creates proxies for our classes in runtime.
+     * we have to use a specific module for Hibernate that we can use in
+     * the ObjectMapper objects and create a bean for the new Hibernate module
+     * for Jackson.
+     * Spring Boot's Jackson2ObjectMapperBuilder will use it via autoconfiguration,
+     * and all the ObjectMapper instances will use the Spring Boot defaults plus
+     * our own customization.
+     * @return
+     */
     @Bean
     public Hibernate5Module hibernateModule() {
         return new Hibernate5Module();
